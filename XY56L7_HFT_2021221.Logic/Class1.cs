95905a09aa44f5e5386cb30b1706338329332082
XY56L7@stud.uni-obuda.hu <XY56L7@stud.uni-obuda.hu>
2021-10-06 00:37:07 +0200

    mdf and ldf removed from gitignore

---
+++ XY56L7_HFT_2021221.Logic/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace XY56L7_HFT_2021221.Logic
+{
+    public class Class1
+    {
+    }
+}
---
+++ XY56L7_HFT_2021221.Repository/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace XY56L7_HFT_2021221.Repository
+{
+    public class Class1
+    {
+    }
+}
